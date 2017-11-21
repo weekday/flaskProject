5f8bd7451ed083d382603c75f04a3b4ad99d0444
weekday <lyj901188@163.com>
2017-11-21 18:21:02 +0800

    REST API

--- app/__init__.py
+++ app/__init__.py
@@ -37,4 +37,7 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix='/auth')
 
+    from .api import api as api_buleprint
+    app.register_blueprint(api_buleprint,url_prefix='/api')
+
     return app
--- app/main/errors.py
+++ app/main/errors.py
@@ -1,16 +1,32 @@
 #coding:utf-8
 
 from . import main
-from flask import render_template
+from flask import render_template, request, jsonify
+
 
 @main.app_errorhandler(403)
 def page_not_fount(e):
+    if request.accept_mimetypes.accept_json and \
+        not request.accept_mimetypes.accept_html:
+        response = jsonify({'error':'forbbiden'})
+        response.status_code = 403
+        return response
     return render_template('403.html'),403
 
 @main.app_errorhandler(404)
 def page_not_fount(e):
+    if request.accept_mimetypes.accept_json and \
+        not request.accept_mimetypes.accept_html:
+        response = jsonify({'error':'page not fount'})
+        response.status_code = 404
+        return response
     return render_template('404.html'),404
 
 @main.app_errorhandler(500)
 def Internel_server_error(e):
+    if request.accept_mimetypes.accept_json and \
+        not request.accept_mimetypes.accept_html:
+        response = jsonify({'error':'internal server error'})
+        response.status_code = 500
+        return response
     return render_template('500.html'),500
