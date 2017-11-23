f03a9e8327c00ba47d3350247bfda5e344db8ceb
weekday <lyj901188@163.com>
2017-11-23 22:49:28 +0800

    正在增加celery分布式异步任务队列。。。。。

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_pagedown import PageDown
+from flask_celery import Celery
 
 
 bootstrap = Bootstrap()
@@ -15,6 +16,7 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 pagedown = PageDown()
+celery = Celery()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -30,6 +32,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     pagedown.init_app(app)
+    celery.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
